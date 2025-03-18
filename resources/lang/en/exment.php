3f8ff811b9c79d1ddcc05f3e4459d2c7d6c20d7f
Zushi <t-zushi@st-m.jp>
2025-03-18 17:55:40 +0900

    add resources/lang/en
    'create_expansion' => 'Create expansion view new',
    'create_expansion' => 'Create a new accordion view that relationship parent and child.',
    'expansion' => 'Expansion view',

--- resources/lang/en/exment.php
+++ resources/lang/en/exment.php
@@ -1902,6 +1902,7 @@ return [
             'create_sum' => 'Create Summary View',
             'create_calendar' => 'Calendar view new creation',
             'create_filter' => 'Create condition view new',
+            'create_expansion' => 'Create expansion view new',
             
             'help' => [
                 'current_view_edit' => 'Change the settings of the currently displayed view.',
@@ -1910,6 +1911,7 @@ return [
                 'create_sum' => 'Create a new aggregation view that groups data items and aggregates and displays the total and maximum values.',
                 'create_calendar' => 'Create a new calendar view that displays the date of data in calendar format.',
                 'create_filter' => 'Create a new condition view that sets the conditions for notification and the conditions to be displayed in the form options.',
+                'create_expansion' => 'Create a new accordion view that relationship parent and child.',
             ],
         ],
         'message' => [
@@ -1928,6 +1930,7 @@ return [
             'default' => 'List view',
             'aggregate' => 'Summary view',
             'calendar' => 'Calendar view',
+            'expansion' => 'Expansion view',
             'filter' => 'Filter view',
             'plugin' => 'Unique view',
             'alldata' => 'All data view',
