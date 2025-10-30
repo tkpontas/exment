c9842e626345e54987e94092ca4ff5e195722b5a
ex1anhth+quangdv <anhth@trithucmoi.co>
2025-10-30 09:55:55 +0700

    change jquery

--- resources/views/install/layout.blade.php
+++ resources/views/install/layout.blade.php
@@ -33,7 +33,7 @@
   </div>
 </div>
   <!-- jQuery (kept for iCheck and toastr) -->
-  <script src="{{ admin_asset('/vendor/open-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+  <script src="{{ admin_asset('/vendor/open-admin/AdminLTE/plugins/jQuery/jQuery-3.7.1.min.js') }}"></script>
   <!-- Bootstrap 5 JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <!-- iCheck (kept for now, but consider replacing with native Bootstrap 5 form controls) -->
