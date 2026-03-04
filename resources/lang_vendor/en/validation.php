5e48f98f7cdd0669608205f817d116c3e63ab715
manhhh1108 <duongmanh2106@gmail.com>
2026-03-04 14:24:16 +0700

    fix: refactor validation.php

--- resources/lang_vendor/en/validation.php
+++ resources/lang_vendor/en/validation.php
@@ -1,14 +1,5 @@
 <?php
 
-// Published by Exment (tag: lang_vendor) into resources/lang/en/validation.php
-
-$vendorValidationPath = base_path('vendor/laravel/framework/src/Illuminate/Translation/lang/en/validation.php');
-
-$defaults = file_exists($vendorValidationPath)
-    ? require $vendorValidationPath
-    : [];
-
-return array_replace_recursive($defaults, [
-    // Exment custom validator rule (uniqueInTable -> unique_in_table)
+return [
     'unique_in_table' => 'The :attribute has already been taken.',
-]);
+];
