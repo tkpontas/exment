33940569dd610fa0bd30c84c894a4cac39f2ee8a
Zushi <t-zushi@st-m.jp>
2026-09-10 20:05:46 +0900

    Update email.blade.php

--- resources/views/auth/email.blade.php
+++ resources/views/auth/email.blade.php
@@ -1,6 +1,6 @@
 @extends('exment::auth.layout') 
 @section('content')
-            <p class="login-box-msg">{{ exmtrans('login.password_reset') }}</p>
+            <p class="login-box-msg">利用者パスワード初期化</p>
     
             <form action="{{ route('password.email') }}" method="post">
                 <div class="form-group has-feedback {!! !$errors->has('email') ?: 'has-error' !!}">
@@ -9,19 +9,19 @@
                     <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{$message}}</label></br>
                     @endforeach @endif
     
-                    <input type="text" class="form-control" placeholder="{{ exmtrans('user.email') }}" name="email" value="{{ old('email') }}" required>
+                    <input type="text" class="form-control" placeholder="認証用メールアドレス" name="email" value="{{ old('email') }}" required>
                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                 </div>  
                 <div class="row">
                     <!-- /.col -->
                     <div class="col-xs-8 col-md-offset-2">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat submit_disabled">{{ exmtrans('login.password_reset') }}</button>
+                        <button type="submit" class="btn btn-dropbox btn-block btn-flat submit_disabled">初期化用URLを送信</button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
             <div style="margin:10px 0; text-align:center;">
-                <p><a href="{{admin_url('auth/login')}}">{{ exmtrans('login.back_login_page') }}</a></p>
+                <p><a href="{{admin_url('auth/login')}}">利用者ログイン画面に戻る</a></p>
             </div>
 @endsection
