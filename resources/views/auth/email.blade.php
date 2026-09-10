1cdfbd55ae0ad6a360e26985e8cc6da179b51308
Zushi <t-zushi@st-m.jp>
2026-09-10 20:15:18 +0900

    Update email.blade.php

--- resources/views/auth/email.blade.php
+++ resources/views/auth/email.blade.php
@@ -1,7 +1,7 @@
 @extends('exment::auth.layout') 
 @section('content')
-            <p class="login-box-msg">利用者パスワード初期化</p>
-    
+            <p class="login-box-msg" style="border-bottom: 2px solid green; padding: 0 0 5px; margin: 0px; font-weight: bold;">利用者パスワード初期化</p>
+    　　<p>パスワード初期化用のURLを認証用メールアドレスに送信します。</p>
             <form action="{{ route('password.email') }}" method="post">
                 <div class="form-group has-feedback {!! !$errors->has('email') ?: 'has-error' !!}">
     
@@ -16,12 +16,12 @@
                     <!-- /.col -->
                     <div class="col-xs-8 col-md-offset-2">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <button type="submit" class="btn btn-dropbox btn-block btn-flat submit_disabled">初期化用URLを送信</button>
+                        <button type="submit" class="btn btn-dropbox btn-block btn-flat submit_disabled"><span class="glyphicon glyphicon-send"></span>　初期化用URLを送信</button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
             <div style="margin:10px 0; text-align:center;">
-                <p><a href="{{admin_url('auth/login')}}">利用者ログイン画面に戻る</a></p>
+                <p><a href="{{admin_url('auth/login')}}"><span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span> 利用者ログイン画面に戻る</a></p>
             </div>
 @endsection
